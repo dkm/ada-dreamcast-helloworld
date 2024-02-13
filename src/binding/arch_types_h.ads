89e167c169184388f3e93f6099d1b0cd01d4cacd
Marc Poulhiès <dkm@kataplop.net>
2024-02-13 21:42:47 +0100

    WIP: get rid of alire

--- src/binding/arch_types_h.ads
+++ src/binding/arch_types_h.ads
@@ -14,88 +14,93 @@ package arch_types_h is
   --   (c)2000-2001 Megan Potter
   -- 
 
-  --* \file   arch/types.h
-  --    \brief  Common integer types.
+  --* \file    arch/types.h
+  --    \brief   Common integer types.
+  --    \ingroup system_types
   --    This file contains typedefs for some common/useful integer types. These
   --    types include ones that tell you exactly how long they are, as well as some
   --    BSD-isms.
   --    \author Megan Potter
   -- 
 
+  --* \addtogroup system_types
+  --    @{
+  -- 
+
   -- Generic types  
   --*< \brief 64-bit unsigned integer  
-   subtype uint64 is Extensions.unsigned_long_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:27
+   subtype uint64 is Extensions.unsigned_long_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:32
 
   --*< \brief 32-bit unsigned integer  
-   subtype uint32 is unsigned_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:28
+   subtype uint32 is unsigned_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:33
 
   --*< \brief 16-bit unsigned integer  
-   subtype uint16 is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:29
+   subtype uint16 is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:34
 
   --*< \brief 8-bit unsigned integer  
-   subtype uint8 is unsigned_char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:30
+   subtype uint8 is unsigned_char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:35
 
   --*< \brief 64-bit signed integer  
-   subtype int64 is Long_Long_Integer;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:31
+   subtype int64 is Long_Long_Integer;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:36
 
   --*< \brief 32-bit signed integer  
-   subtype int32 is long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:32
+   subtype int32 is long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:37
 
   --*< \brief 16-bit signed integer  
-   subtype int16 is short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:33
+   subtype int16 is short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:38
 
   --*< \brief 8-bit signed integer  
-   subtype int8 is char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:34
+   subtype int8 is char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:39
 
   -- Volatile types  
   --*< \brief 64-bit volatile unsigned type  
-   subtype vuint64 is uint64;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:37
+   subtype vuint64 is uint64;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:42
 
   --*< \brief 32-bit volatile unsigned type  
-   subtype vuint32 is uint32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:38
+   subtype vuint32 is uint32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:43
 
   --*< \brief 16-bit volatile unsigned type  
-   subtype vuint16 is uint16;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:39
+   subtype vuint16 is uint16;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:44
 
   --*< \brief 8-bit volatile unsigned type  
-   subtype vuint8 is uint8;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:40
+   subtype vuint8 is uint8;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:45
 
   --*< \brief 64-bit volatile signed type  
-   subtype vint64 is int64;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:41
+   subtype vint64 is int64;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:46
 
   --*< \brief 32-bit volatile signed type  
-   subtype vint32 is int32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:42
+   subtype vint32 is int32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:47
 
   --*< \brief 16-bit volatile signed type  
-   subtype vint16 is int16;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:43
+   subtype vint16 is int16;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:48
 
   --*< \brief 8-bit volatile signed type  
-   subtype vint8 is int8;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:44
+   subtype vint8 is int8;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:49
 
   -- Pointer arithmetic types  
   --*< \brief Pointer arithmetic type  
-   subtype ptr_t is uint32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:47
+   subtype ptr_t is uint32;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:52
 
   -- another format for type names  
   -- \cond  
   -- \endcond  
   --*< \brief BSD-style unsigned char  
-   subtype u_char is unsigned_char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:61
+   subtype u_char is unsigned_char;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:66
 
   --*< \brief BSD-style unsigned short  
-   subtype u_short is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:62
+   subtype u_short is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:67
 
   --*< \brief BSD-style unsigned integer  
-   subtype u_int is unsigned;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:63
+   subtype u_int is unsigned;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:68
 
   --*< \brief BSD-style unsigned long  
-   subtype u_long is unsigned_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:64
+   subtype u_long is unsigned_long;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:69
 
   --*< \brief BSD-style unsigned short  
-   subtype ushort is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:65
+   subtype ushort is unsigned_short;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:70
 
   --*< \brief BSD-style unsigned integer  
-   subtype uint is unsigned;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:66
+   subtype uint is unsigned;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:71
 
   -- \cond  
   -- \endcond  
@@ -104,17 +109,18 @@ package arch_types_h is
   --   restraints.  
 
   --*< \brief Generic "handle" type  
-   subtype handle_t is int;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:77
+   subtype handle_t is int;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:82
 
   -- Thread and priority types  
   --*< \brief Thread ID type  
-   subtype tid_t is handle_t;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:80
+   subtype tid_t is handle_t;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:85
 
   --*< \brief Priority value type  
-   subtype prio_t is handle_t;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:81
+   subtype prio_t is handle_t;  -- /mnt/barryallen/dkm/git/KallistiOS/kernel/arch/dreamcast/include/arch/types.h:86
 
   -- Make sure to pull in the base endianness defines...  
   --* \brief  Endianness definition -- Little Endian  
+  --* @}  
 end arch_types_h;
 
 pragma Style_Checks (On);
--- src/dchelloword.adb
+++ src/dchelloword.adb
@@ -1,32 +1,61 @@
-with dc_biosfont_h;  use dc_biosfont_h;
+-- with dc_biosfont_h;  use dc_biosfont_h;
+with dc_minifont_h; use dc_minifont_h;
 with System;
 with System.Storage_Elements; use System.Storage_Elements;
+with Interfaces.C; use Interfaces.C;
 with Interfaces.C.Strings; use Interfaces.C.Strings;
+with Interfaces; use Interfaces;
+with arch_types_h; use arch_types_h;
 
 procedure Dchelloword is
-   type Uint16_T is mod 2 ** 16;
-   type Vram_T is array (Storage_Offset range 0 .. 8 * 1024 * 1024)
-     of Uint16_T;
+--   type Uint16_T is mod 2 ** 16;
+
+   --  Dreamcast VRAM is 8M and abstracted as uint16
+   type Vram_T is array (Storage_Offset range 0 .. (8 * 1024 * 1024) / 2)
+     of aliased Unsigned_16;
 
    Vram_S : System.Address with
      Import,
      External_Name => "vram_s";
 
-   Vram_Data : Vram_T;
+   type Vram_2_T is array (Storage_Offset range 0 .. (8 * 1024 * 1024) / 2)
+     of aliased uint16;
+
+   Vram_Data : aliased Vram_T;
    for Vram_Data'Address use Vram_S;
 
-   Offset : Storage_Offset := (640 * BFONT_HEIGHT + BFONT_THIN_WIDTH * 2);
+
+   Vram_Data_2 : aliased Vram_2_T;
+   for Vram_Data_2'Address use Vram_S;
+
+   -- Offset : Storage_Offset := (640 * BFONT_HEIGHT + BFONT_THIN_WIDTH * 2);
+
+   C : Unsigned_16;
+   --  pragma Import (Intrinsic, Shift_Left);
+   Ignore : Interfaces.C.int;
 
 begin
-   bfont_set_encoding (BFONT_CODE_ISO8859_1);
-   bfont_draw_str (
-     Vram_Data (Offset)'Address,
-     --  Vram_S + Offset,
-     -- b=0xa5007830
-     640,
-     1,
-     New_String ("Test of basic ASCII")
-   );
+   for Y in 0 .. 480 loop
+      for X in 0 .. 640 loop
+         C := (Unsigned_16 (X) xor Unsigned_16 (Y)) mod 255;
+         Vram_Data (Storage_Offset (Y * 640 + X)) := Shift_Left (Shift_Right (C, 3),  12)
+           or Shift_Left ( Shift_Right (C, 2), 5)
+           or Shift_Left ( Shift_Right (C, 3), 0);
+      end loop;
+   end loop;
+
+   Ignore := minifont_draw_str(Vram_Data_2 (Storage_Offset (20*640+20))'Access, 640,
+      New_String ("Hello, World for Ada!!"));
+
+   --  bfont_set_encoding (BFONT_CODE_ISO8859_1);
+   --  bfont_draw_str (
+   --    Vram_Data (Offset)'Address,
+   --    --  Vram_S + Offset,
+   --    -- b=0xa5007830
+   --    640,
+   --    1,
+   --    New_String ("Test of basic ASCII")
+   --  );
 
    while True loop
       null;
