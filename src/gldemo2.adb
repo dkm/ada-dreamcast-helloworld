818c666e8b5dfd9187b44093c2db22433ce449c2
Marc Poulhiès <dkm@kataplop.net>
2024-02-21 19:56:27 +0100

    WIP

--- src/gldemo2.adb
+++ src/gldemo2.adb
@@ -7,6 +7,11 @@ with gl_glkos_h; use gl_glkos_h;
 with gl_glu_h; use gl_glu_h;
 with gl_glext_h; use gl_glext_h;
 
+--  This is mostly an Ada port of the new cargo-cube Rust example:
+--  https://github.com/darcagn/rust-for-dreamcast/tree/master/examples/cargo-cube
+--
+--  The controller handling hasn't been ported and the program never ends.
+
 procedure Gldemo2 is
 
    --  VQ 512x512 byte array
@@ -66,21 +71,21 @@ procedure Gldemo2 is
 begin
    glKosInit;
 
-   glMatrixMode(GL_PROJECTION);
+   glMatrixMode (GL_PROJECTION);
    glLoadIdentity;
-   gluPerspective(45.0, 640.0 / 480.0, 0.1, 100.0);
-   glMatrixMode(GL_MODELVIEW);
+   gluPerspective (45.0, 640.0 / 480.0, 0.1, 100.0);
+   glMatrixMode (GL_MODELVIEW);
    glLoadIdentity;
 
-   glEnable(GL_TEXTURE_2D);
-   glShadeModel(GL_SMOOTH);
-   glClearColor(0.0, 0.0, 0.0, 0.5);
-   glClearDepth(1.0);
-   glEnable(GL_DEPTH_TEST);
-   glDepthFunc(GL_LEQUAL);
+   glEnable (GL_TEXTURE_2D);
+   glShadeModel (GL_SMOOTH);
+   glClearColor (0.0, 0.0, 0.0, 0.5);
+   glClearDepth (1.0);
+   glEnable (GL_DEPTH_TEST);
+   glDepthFunc (GL_LEQUAL);
 
-   glGenTextures(1, Texture_Ada1'Access);
-   glBindTexture(GL_TEXTURE_2D, Texture_Ada1);
+   glGenTextures (1, Texture_Ada1'Access);
+   glBindTexture (GL_TEXTURE_2D, Texture_Ada1);
    glCompressedTexImage2DARB
      (GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_565_VQ_TWID_KOS,
       512, 512, 0, Texture_Ada1_Data'Size,
@@ -93,142 +98,142 @@ begin
       512, 512, 0, Texture_Dc_Data'Size,
       Texture_Dc_Data'Address);
 
-   glGenTextures(1, Texture_Dcwiki'Access);
-   glBindTexture(GL_TEXTURE_2D, Texture_Dcwiki);
+   glGenTextures (1, Texture_Dcwiki'Access);
+   glBindTexture (GL_TEXTURE_2D, Texture_Dcwiki);
    glCompressedTexImage2DARB
      (GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_565_VQ_TWID_KOS,
       512, 512, 0, Texture_Dcwiki_Data'Size,
       Texture_Dcwiki_Data'Address);
 
-   glGenTextures(1, Texture_Gcc'Access);
-   glBindTexture(GL_TEXTURE_2D, Texture_Gcc);
+   glGenTextures (1, Texture_Gcc'Access);
+   glBindTexture (GL_TEXTURE_2D, Texture_Gcc);
    glCompressedTexImage2DARB
      (GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_565_VQ_TWID_KOS,
       512, 512, 0, Texture_Gcc_Data'Size,
       Texture_Gcc_Data'Address);
 
-   glGenTextures(1, Texture_Kos'Access);
-   glBindTexture(GL_TEXTURE_2D, Texture_Kos);
+   glGenTextures (1, Texture_Kos'Access);
+   glBindTexture (GL_TEXTURE_2D, Texture_Kos);
    glCompressedTexImage2DARB
      (GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_565_VQ_TWID_KOS,
       512, 512, 0, Texture_Kos_Data'Size,
       Texture_Kos_Data'Address);
 
-   glGenTextures(1, Texture_Ada'Access);
-   glBindTexture(GL_TEXTURE_2D, Texture_Ada);
+   glGenTextures (1, Texture_Ada'Access);
+   glBindTexture (GL_TEXTURE_2D, Texture_Ada);
    glCompressedTexImage2DARB
      (GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB_565_VQ_TWID_KOS,
       512, 512, 0, Texture_Ada_Data'Size,
       Texture_Ada_Data'Address);
 
    loop
-       glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT);
+       glClear (GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT);
        glLoadIdentity;
-       glTranslatef(0.0, 0.0, -5.0);
+       glTranslatef (0.0, 0.0, -5.0);
 
-       glRotatef(X_Rot, 1.0, 0.0, 0.0);
-       glRotatef(Y_Rot, 0.0, 1.0, 0.0);
-       glRotatef(Z_Rot, 0.0, 0.0, 1.0);
+       glRotatef (X_Rot, 1.0, 0.0, 0.0);
+       glRotatef (Y_Rot, 0.0, 1.0, 0.0);
+       glRotatef (Z_Rot, 0.0, 0.0, 1.0);
 
        --  Front face
-       glBindTexture(GL_TEXTURE_2D, Texture_Ada1);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Ada1);
+       glBegin (GL_QUADS);
 
-       glColor3f(1.0, 1.0, 1.0);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(-1.0, -1.0,  1.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(1.0, -1.0,  1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(1.0,  1.0,  1.0);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(-1.0,  1.0,  1.0);
+       glColor3f (1.0, 1.0, 1.0);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (-1.0, -1.0,  1.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (1.0, -1.0,  1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (1.0,  1.0,  1.0);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (-1.0,  1.0,  1.0);
 
        glEnd;
 
        --  Back face
-       glBindTexture(GL_TEXTURE_2D, Texture_Gcc);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Gcc);
+       glBegin (GL_QUADS);
 
-       glColor3f(1.0, 1.0, 1.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(-1.0, -1.0, -1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(-1.0,  1.0, -1.0);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(1.0,  1.0, -1.0);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(1.0, -1.0, -1.0);
+       glColor3f (1.0, 1.0, 1.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (-1.0, -1.0, -1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (-1.0,  1.0, -1.0);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (1.0,  1.0, -1.0);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (1.0, -1.0, -1.0);
 
        glEnd;
 
        --  Top face
-       glBindTexture(GL_TEXTURE_2D, Texture_Dcwiki);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Dcwiki);
+       glBegin (GL_QUADS);
 
-       glColor3f(1.0, 1.0, 1.0);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(-1.0,  1.0, -1.0);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(-1.0,  1.0,  1.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(1.0,  1.0,  1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(1.0,  1.0, -1.0);
+       glColor3f (1.0, 1.0, 1.0);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (-1.0,  1.0, -1.0);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (-1.0,  1.0,  1.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (1.0,  1.0,  1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (1.0,  1.0, -1.0);
 
        glEnd;
 
        --  Bottom face
-       glBindTexture(GL_TEXTURE_2D, Texture_Kos);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Kos);
+       glBegin (GL_QUADS);
 
-       glColor3f(1.0, 1.0, 1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(-1.0, -1.0, -1.0);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(1.0, -1.0, -1.0);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(1.0, -1.0,  1.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(-1.0, -1.0,  1.0);
+       glColor3f (1.0, 1.0, 1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (-1.0, -1.0, -1.0);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (1.0, -1.0, -1.0);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (1.0, -1.0,  1.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (-1.0, -1.0,  1.0);
 
        glEnd;
 
        --  Right face
-       glBindTexture(GL_TEXTURE_2D, Texture_Dc);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Dc);
+       glBegin (GL_QUADS);
 
-       glColor3f(0.0, 1.0, 0.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(1.0, -1.0, -1.0);
-       glColor3f(0.3, 0.5, 1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(1.0,  1.0, -1.0);
-       glColor3f(1.0, 0.3, 0.5);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(1.0,  1.0,  1.0);
-       glColor3f(0.5, 0.5, 0.5);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(1.0, -1.0,  1.0);
+       glColor3f (0.0, 1.0, 0.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (1.0, -1.0, -1.0);
+       glColor3f (0.3, 0.5, 1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (1.0,  1.0, -1.0);
+       glColor3f (1.0, 0.3, 0.5);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (1.0,  1.0,  1.0);
+       glColor3f (0.5, 0.5, 0.5);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (1.0, -1.0,  1.0);
 
        glEnd;
 
        --  Left face
-       glBindTexture(GL_TEXTURE_2D, Texture_Ada);
-       glBegin(GL_QUADS);
+       glBindTexture (GL_TEXTURE_2D, Texture_Ada);
+       glBegin (GL_QUADS);
 
-       glColor3f(1.0, 0.0, 0.0);
-       glTexCoord2f(1.0, 0.0);
-       glVertex3f(-1.0, -1.0, -1.0);
-       glColor3f(1.0, 1.0, 0.0);
-       glTexCoord2f(0.0, 0.0);
-       glVertex3f(-1.0, -1.0,  1.0);
-       glColor3f(0.0, 1.0, 1.0);
-       glTexCoord2f(0.0, 1.0);
-       glVertex3f(-1.0,  1.0,  1.0);
-       glColor3f(0.0, 0.0, 1.0);
-       glTexCoord2f(1.0, 1.0);
-       glVertex3f(-1.0,  1.0, -1.0);
+       glColor3f (1.0, 0.0, 0.0);
+       glTexCoord2f (1.0, 0.0);
+       glVertex3f (-1.0, -1.0, -1.0);
+       glColor3f (1.0, 1.0, 0.0);
+       glTexCoord2f (0.0, 0.0);
+       glVertex3f (-1.0, -1.0,  1.0);
+       glColor3f (0.0, 1.0, 1.0);
+       glTexCoord2f (0.0, 1.0);
+       glVertex3f (-1.0,  1.0,  1.0);
+       glColor3f (0.0, 0.0, 1.0);
+       glTexCoord2f (1.0, 1.0);
+       glVertex3f (-1.0,  1.0, -1.0);
 
        glEnd;
 
@@ -240,11 +245,11 @@ begin
    end loop;
 
    -- Clean up our textures
-   glDeleteTextures(1, Texture_Ada1'Access);
-   glDeleteTextures(1, Texture_Dc'Access);
-   glDeleteTextures(1, Texture_Dcwiki'Access);
-   glDeleteTextures(1, Texture_Gcc'Access);
-   glDeleteTextures(1, Texture_Kos'Access);
-   glDeleteTextures(1, Texture_Ada'Access);
+   glDeleteTextures (1, Texture_Ada1'Access);
+   glDeleteTextures (1, Texture_Dc'Access);
+   glDeleteTextures (1, Texture_Dcwiki'Access);
+   glDeleteTextures (1, Texture_Gcc'Access);
+   glDeleteTextures (1, Texture_Kos'Access);
+   glDeleteTextures (1, Texture_Ada'Access);
 
 end Gldemo2;
