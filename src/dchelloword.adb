86eac05748646b2ae72060031662fbc5a755d34d
Marc Poulhiès <dkm@kataplop.net>
2024-02-22 21:19:54 +0100

    WIP: make it more Ada

--- src/dchelloword.adb
+++ src/dchelloword.adb
@@ -1,3 +1,5 @@
+with Ada.Text_IO;  use Ada.Text_IO;
+
 with System;
 with System.Storage_Elements; use System.Storage_Elements;
 with Interfaces.C; use Interfaces.C;
@@ -8,40 +10,48 @@ with arch_types_h; use arch_types_h;
 with dc_minifont_h; use dc_minifont_h;
 
 procedure Dchelloword is
-   --  Dreamcast VRAM is 8M and abstracted as uint16
-   type Vram_T is array (Storage_Offset range 0 .. (8 * 1024 * 1024) / 2)
-     of aliased Unsigned_16;
 
+   --  A frame buffer stored in row-major for 640x480.
+   type Frame_Buffer is array
+     (Storage_Offset range 1 .. 480,
+      Storage_Offset range 1 .. 640)
+     of aliased uint16 with Pack;
+
+   --  This is the base address of the VRAM
    Vram_S : System.Address with
      Import,
      External_Name => "vram_s";
 
-   type Vram_2_T is array (Storage_Offset range 0 .. (8 * 1024 * 1024) / 2)
-     of aliased uint16;
+   --  This is the visible framebuffer
+   FB : Frame_Buffer;
+   for FB'Address use Vram_S;
 
-   Vram_Data : aliased Vram_T;
-   for Vram_Data'Address use Vram_S;
-
-   Vram_Data_2 : aliased Vram_2_T;
-   for Vram_Data_2'Address use Vram_S;
-
-   C : Unsigned_16;
    Ignore : Interfaces.C.int;
 
-begin
-   for Y in 0 .. 480 loop
-      for X in 0 .. 640 loop
-         C := (Unsigned_16 (X) xor Unsigned_16 (Y)) mod 255;
-         Vram_Data (Storage_Offset (Y * 640 + X)) := Shift_Left (Shift_Right (C, 3),  12)
+   function Get_Pixel_Color (Line : Storage_Offset; Col : Storage_Offset)
+     return uint16
+   is
+      C : Unsigned_16 := (Unsigned_16 (Col) xor Unsigned_16 (Line)) mod 255;
+   begin
+      return uint16 (Shift_Left (Shift_Right (C, 3),  12)
            or Shift_Left ( Shift_Right (C, 2), 5)
-           or Shift_Left ( Shift_Right (C, 3), 0);
+           or Shift_Left ( Shift_Right (C, 3), 0));
+   end Get_Pixel_Color;
+
+begin
+   Put_Line ("Hello from Ada!");
+   Put_Line ("This is some Ada.Text_IO text :)");
+
+   for Line in FB'Range (1) loop
+      for Column in FB'Range (2) loop
+         FB (Line, Column) := Get_Pixel_Color (Line, Column);
       end loop;
    end loop;
 
-   Ignore := minifont_draw_str(Vram_Data_2 (Storage_Offset (20*640+20))'Access, 640,
+   Ignore := minifont_draw_str(FB(20, 20)'Access, 640,
       New_String ("Hello, World. This is Ada!!"));
 
-   Ignore := minifont_draw_str(Vram_Data_2 (Storage_Offset (80*640+20))'Access, 640,
+   Ignore := minifont_draw_str(FB (80, 20)'Access, 640,
       New_String ("And Ada is older than this Dreamcast!!"));
 
    while True loop
